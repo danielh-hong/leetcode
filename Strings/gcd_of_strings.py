21b15c9561b91743a4e51623bfb15de2fc2218f8
danielh-hong <danielh.toronto@gmail.com>
2025-01-20 22:26:51 -0500

    Completed kids with most candies

--- Strings/gcd_of_strings.py
+++ Strings/gcd_of_strings.py
@@ -1,5 +1,5 @@
-# GCD of strings
 
+# GCD of strings (easy) (completed)
 '''
 For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
 
