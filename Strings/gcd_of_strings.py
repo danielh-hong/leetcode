3aecd8650785b464ac5817ba3ac7d512fd2e0605
danielh-hong <danielh.toronto@gmail.com>
2025-01-20 22:22:41 -0500

    GCD of strings. Easy. Strings

---
+++ Strings/gcd_of_strings.py
@@ -0,0 +1,38 @@
+# GCD of strings
+
+'''
+For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+ 
+ '''
+
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        if str1 + str2 != str2 + str1:
+            return ""
+
+        ret = 0
+        for i in range(1, len(min(str1, str2)) + 1):
+            if len(str1) % i == 0 and len(str2) % i == 0:
+                ret = i
+
+        return str1[:ret]
